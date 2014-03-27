9a571baa50cc7fafdb619271ff07ef96485c4275
Kenji Ohno <kenjiohno@Kenji-no-MacBook-Pro.local>
2014-03-28 08:24:45 +0900

    Initial Commit

---
+++ konashiSensorShield/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  konashiSensorShield
+//
+//  Created by Kenji Ohno on 2014/03/28.
+//  Copyright (c) 2014年 Macnica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ konashiSensorShield/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  konashiSensorShield
+//
+//  Created by Kenji Ohno on 2014/03/28.
+//  Copyright (c) 2014年 Macnica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
